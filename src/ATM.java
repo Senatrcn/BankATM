b1456e5481c8dbafb4ea3e82d197dbcd5047da36
Sena TERCAN <avsenatercan@gmail.com>
2022-04-10 23:23:33 +0300

    Main

--- src/ATM.java
+++ src/ATM.java
@@ -4,10 +4,6 @@ public class ATM {
     public static void main(String[] args) {
 
         Methods.atmGiris();
-
-        //Methods.hesapOlustur();
-        //Methods.hesapOlustur();
-        //System.out.println(Methods.hesapListesi);
     }
 
 }
